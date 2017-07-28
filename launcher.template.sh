bd2bf22a650786d68473208a30d067fccfef47fb
David R. Smith <drsmith8@wisc.edu>
2017-07-27 22:49:25 -0400

    removed local directories

--- launcher.template.sh
+++ launcher.template.sh
@@ -28,40 +28,8 @@ set sourcefile="/p/gene/drsmith/gene-pgf.csh"
 echo "Sourcing ${sourcefile}"
 source ${sourcefile}
 
-set genedir="/local/drsmith-${SLURM_JOB_NAME}-${SLURM_JOB_ID}"
-mkdir ${genedir}
-cd ${genedir}
-ln -s ${GENEHOME}/tools tools
-ln -s ${GENEHOME}/bin bin
-ln -s ${GENEHOME}/makefile makefile
-ln -s ${GENEHOME}/makefiles makefiles
-ln -s ${GENEHOME}/.git .git
-ln -s ${GENEHOME}/src src
-ls -al
-
-mkdir probdir
-cd probdir
-
-##set localdir="/local/drsmith-${SLURM_JOB_NAME}-${SLURM_JOB_ID}"
-##echo "Creating and move to local dir ${localdir}"
-##mkdir ${localdir}
-##cd ${localdir}
-##pwd
-
-echo "Creating sym links in local problem dir"
-ln -s ${SLURM_SUBMIT_DIR}/parameters parameters
-ln -s ${SLURM_SUBMIT_DIR}/scanscript scanscript
-ln -s ${SLURM_SUBMIT_DIR}/gene_pppl_pgf gene_pppl_pgf
-ls -l
-
 echo "Running scanscript"
 ./scanscript --np={{ tasks|default('64', true) }} --ppn=32 --mps=4
-###${SLURM_SUBMIT_DIR}/scanscript --np={{ tasks|default('64', true) }} --ppn=32 --mps=4
-
-echo "Copy scanfiles*/ to ${SLURM_SUBMIT_DIR}"
-cp -fR scanfiles*/ ${SLURM_SUBMIT_DIR}
-ls -l
-ls -l ${SLURM_SUBMIT_DIR}
 
 echo "Finished"
 exit
